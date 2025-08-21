e67fe43e2a6e78312b8929a4c15606c9c6279f5c
DmitriyOne <senkoner4@gmail.com>
2025-08-21 22:01:41 +0100

    [Backend] Update bootstrap

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,29 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { PgExceptionFilter } from './filters';
+import { ConfigService } from '@nestjs/config';
+import { TEnvConfig } from './schema';
 
 async function bootstrap() {
+  const logger = new Logger('Bootstrap');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 4000);
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new PgExceptionFilter(httpAdapter));
+
+  const configService = app.get(ConfigService<TEnvConfig>);
+
+  const port = configService.get<number>('PORT');
+  await app.listen(port!);
+  logger.log(`Server is running on port: ${port}`);
+
+  const url = await app.getUrl();
+  logger.log(`The application is running on: ${url}`);
 }
 
-bootstrap();
+bootstrap().catch((err) => {
+  console.error('Error during application bootstrap: ', err);
+  process.exit(1);
+});
